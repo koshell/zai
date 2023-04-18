f0bdbcc2ac4782294c2fe3958e38b09576b7cc02
zaiju-code <zone1135@gmail.com>
2023-04-19 08:10:54 +1000

    Offload the (error prone) dynamic creation of files for controlled cp method

--- pacman/aur_user.fish
+++ pacman/aur_user.fish
@@ -8,28 +8,31 @@ source "$ZAI_DIR/source/format.fish"
 # Load helper functions
 source "$ZAI_DIR/source/functions.fish"
 
-txt_minor "Creating 'aur' system user to run builds without sudo..."
-if not useradd --create-home --home-dir '/aur' --comment 'AUR package builder' --system aur 2>> "$(_err)" | tee -a "$(_log)" 
+txt_major "Starting creation of 'aur' system user..."
+
+txt_minor "Creating 'aur' system user to run builds without root privileges..."
+useradd \
+	--create-home \
+	--home-dir '/aur' \
+	--comment 'AUR package builder' \
+	--shell /usr/bin/nologin \
+	--system aur 2>> "$(_err)" | tee -a "$(_log)" 
+if test $pipestatus[1] -ne 0
 	err_major "Failed to create 'aur' user"
 	abort
 end
 
-txt_minor "Giving 'aur' user access to pacman commands..."
-# Due to the mess that is 'Arch' PATH we don't actually know for certain
-# which way a given command will be resolved, and it is unclear if 'sudoers'
-# is smart enough to follow the symlinks all the way to the end, so we just
-# allow access to every possible path to 'pacman'
-set _sudoers (string join '' \
-	'aur ALL=(ALL) NOPASSWD:/bin/pacman *\n' \
-	'aur ALL=(ALL) NOPASSWD:/sbin/pacman *\n' \
-	'aur ALL=(ALL) NOPASSWD:/usr/bin/pacman *\n' \
-	'aur ALL=(ALL) NOPASSWD:/usr/sbin/pacman *')
-#set _sudoers 'aur ALL = NOPASSWD : ALL'
-mkdir -p -v /etc/sudoers.d >> "$(_log)" 2>> "$(_err)"
-if echo -e $_sudoers > /etc/sudoers.d/12_aur
-	txt_base "Successfully gave 'aur' user access to pacman"
-else
-	err_base "Failed to give 'aur' user access to pacman"
+txt_minor "Creating 'makepkg' system group..."
+groupadd --system --force makepkg 2>> "$(_err)" | tee -a "$(_log)"
+if test $pipestatus[1] -ne 0
+	err_major "Failed to create 'makepkg' group"
+	abort
+end
+
+txt_minor "Ading 'aur' user to 'makepkg' group..."
+usermod --append -G makepkg aur 2>> "$(_err)" | tee -a "$(_log)"
+if test $pipestatus[1] -ne 0
+	err_major "Failed to add user 'aur' to group 'makepkg'"
 	abort
 end
 
@@ -37,22 +40,25 @@ end
 txt_major "Creating directories needed for 'aur' to successfully build packages..."
 
 txt_minor "Creating '/aur/pkgbuild' directory..."
-if su -c "mkdir -p -v /aur/pkgbuild" aur >> "$(_log)" 2>> "$(_err)"
+if su -c "mkdir -pv /aur/pkgbuild" aur >> "$(_log)" 2>> "$(_err)"
 	txt_base "Successfully created '/aur/pkgbuild'" 
 else
 	err_base "Failed to create '/aur/pkgbuild'"
 end
 
 txt_minor "Creating '/aur/built' directory..."
-if su -c "mkdir -p -v /aur/built" aur >> "$(_log)" 2>> "$(_err)"
+if su -c "mkdir -pv /aur/built" aur >> "$(_log)" 2>> "$(_err)"
     txt_base "Successfully created '/aur/built'"
 else
 	err_base "Failed to create '/aur/built'"
 end
 
 txt_minor "Creating '/aur/tmp' directory..."
-if su -c "mkdir -p $(_v) /aur/tmp" aur >> "$(_log)" 2>> "$(_err)"
+if su -c "mkdir -pv /aur/tmp" aur >> "$(_log)" 2>> "$(_err)"
 	txt_base "Successfully created '/aur/tmp'"
 else
 	err_base "Failed to create '/aur/tmp'"
 end
+
+txt_major "Finished creation of 'aur' system user..."
+return
