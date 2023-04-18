674af6aa2d683d27055d746f878d87386ce1fe77
zaiju-code <zone1135@gmail.com>
2023-04-18 15:38:09 +1000

    Added a 'lines of code' total to the readme and a script to update it

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Z.A.I.
 Zaiju's Arch Installer
+
+
+Currently contains a total of 1679 lines of code in a mix of bash and fish.
---
+++ git-utility/readme.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+
+set -x ZAI_DIR '.'
+
+# Load colour and format variables
+source "$ZAI_DIR/source/format.fish"
+
+# Load helper functions
+source "$ZAI_DIR/source/functions.fish"
+
+set file_list (find . -mindepth 1 -type f | \
+    grep -E '(\.bash$)|(\.fish$)|(\.sh$)')
+
+set total_lines (cat $file_list | \
+    grep -vxE '([[:blank:]]*#.*)|([[:blank:]]*)' | \
+    wc -l)
+
+replace_line "a total of [[:digit:]]* lines" \
+    "a total of $total_lines lines" \
+    "$ZAI_DIR/README.md"
+
+
