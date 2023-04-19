db34c32b7a2b56ca021140a52929bd504eda5713
Zaiju <zone1135@gmail.com>
2023-04-20 05:28:51 +1000

    Fix aur user creator

--- pacman/aur_user.fish
+++ pacman/aur_user.fish
@@ -11,27 +11,25 @@ source "$ZAI_DIR/source/functions.fish"
 txt_major "Starting creation of 'aur' system user..."
 
 txt_minor "Creating 'aur' system user to run builds without root privileges..."
-useradd \
-	--create-home \
-	--home-dir '/aur' \
-	--comment 'AUR package builder' \
-	--shell /usr/bin/nologin \
-	--system aur 2>> "$(_err)" | tee -a "$(_log)" 
-if test $pipestatus[1] -ne 0
+if not zai_verbose "$( \
+	useradd \
+		--create-home \
+		--home-dir '/aur' \
+		--comment 'AUR package builder' \
+		--system aur 2>> $(_err) \
+)"
 	err_major "Failed to create 'aur' user"
 	abort
 end
 
 txt_minor "Creating 'makepkg' system group..."
-groupadd --system --force makepkg 2>> "$(_err)" | tee -a "$(_log)"
-if test $pipestatus[1] -ne 0
+if not zai_verbose "$( groupadd --system --force makepkg 2>> $(_err) )"
 	err_major "Failed to create 'makepkg' group"
 	abort
 end
 
-txt_minor "Ading 'aur' user to 'makepkg' group..."
-usermod --append -G makepkg aur 2>> "$(_err)" | tee -a "$(_log)"
-if test $pipestatus[1] -ne 0
+txt_minor "Adding 'aur' user to 'makepkg' group..."
+if not zai_verbose "$( usermod --append -G makepkg aur 2>> $(_err) )"
 	err_major "Failed to add user 'aur' to group 'makepkg'"
 	abort
 end
@@ -40,21 +38,21 @@ end
 txt_major "Creating directories needed for 'aur' to successfully build packages..."
 
 txt_minor "Creating '/aur/pkgbuild' directory..."
-if su -c "mkdir -pv /aur/pkgbuild" aur >> "$(_log)" 2>> "$(_err)"
+if zai_verbose "$( su -c "mkdir -pv /aur/pkgbuild" aur 2>> $(_err) )"
 	txt_base "Successfully created '/aur/pkgbuild'" 
 else
 	err_base "Failed to create '/aur/pkgbuild'"
 end
 
 txt_minor "Creating '/aur/built' directory..."
-if su -c "mkdir -pv /aur/built" aur >> "$(_log)" 2>> "$(_err)"
+if zai_verbose "$( su -c "mkdir -pv /aur/built" aur 2>> $(_err) )"
     txt_base "Successfully created '/aur/built'"
 else
 	err_base "Failed to create '/aur/built'"
 end
 
 txt_minor "Creating '/aur/tmp' directory..."
-if su -c "mkdir -pv /aur/tmp" aur >> "$(_log)" 2>> "$(_err)"
+if zai_verbose "$( su -c "mkdir -pv /aur/tmp" aur 2>> $(_err) )"
 	txt_base "Successfully created '/aur/tmp'"
 else
 	err_base "Failed to create '/aur/tmp'"
