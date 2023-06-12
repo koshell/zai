412b1977e547285704ff3d77ea352a0910e9576e
Zaiju <zone1135@gmail.com>
2023-06-13 07:27:12 +1000

    wip

---
+++ src/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Module Docstring
+"""
+from __future__ import annotations
+
+__author__ = "Zaiju"
+__version__ = "0.1.0"
+__license__ = "GPL3"
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    ...
+
+
+class Config:
+    def __init__(self, config_file):
+        ...
